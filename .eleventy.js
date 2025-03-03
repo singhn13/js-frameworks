4f972034e4f833a430ae02275b303315954b5dfc
Navya Singh <98058638+singhn13@users.noreply.github.com>
2025-03-03 04:05:35 +0000

    first commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
 };
